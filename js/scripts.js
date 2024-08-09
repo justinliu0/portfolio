c6b729f010e8df49f72c329aace67ba70f0c041a
justinliu0 <justinliu103@gmail.com>
2024-08-08 22:57:18 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
   var navbar = document.getElementById('navbar');
+  var scrollToTopBtn = document.querySelector('.scroll-to-top');
 
   window.addEventListener('scroll', function() {
     if (window.scrollY > 0) {
-      navbar.classList.add('scrolled'); 
+      navbar.classList.add('scrolled');
+      if (window.scrollY > 300) {
+        scrollToTopBtn.style.display = 'block';
+      } else {
+        scrollToTopBtn.style.display = 'none';
+      }
     } else {
-      navbar.classList.remove('scrolled'); 
+      navbar.classList.remove('scrolled');
+      scrollToTopBtn.style.display = 'none';
     }
   });
 
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
-      
+
       var targetId = this.getAttribute('href').substring(1);
       var targetElement = document.getElementById(targetId);
 
@@ -22,6 +29,14 @@ document.addEventListener('DOMContentLoaded', function () {
           behavior: 'smooth'
         });
       }
+    });
+  });
+
+  scrollToTopBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
     });
   });
 });
