5967d4273d7fe873f0c18f2d30ff9cdbf69437e4
justinliu0 <justinliu103@gmail.com>
2024-08-08 20:46:35 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
   var navbar = document.getElementById('navbar');
 
   window.addEventListener('scroll', function() {
-    if (window.scrollY === 0) {
-      navbar.classList.add('hidden');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled'); 
     } else {
-      navbar.classList.remove('hidden');
+      navbar.classList.remove('scrolled'); 
     }
   });
 });
