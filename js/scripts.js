0f8875adefd1018cbf231e28631cc2af45fabe7e
justinliu0 <justinliu103@gmail.com>
2024-08-17 16:12:26 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     var skillsRect = skillsSection.getBoundingClientRect();
-    if (skillsRect.top < windowHeight * 0.4 && skillsRect.bottom > windowHeight * 0.4) {
+    if (skillsRect.top < windowHeight * 0.5 && skillsRect.bottom > windowHeight * 0.5) {
       skillDivs.forEach(div => div.classList.add('color-changed'));
     } else {
       skillDivs.forEach(div => div.classList.remove('color-changed'));
