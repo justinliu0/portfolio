dfd186decdeb65222ef5392a9cbbf491f9cee1b8
justinliu0 <justinliu103@gmail.com>
2024-08-19 18:02:37 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
   var scrollToTopBtn = document.querySelector('.scroll-to-top');
   var expandableSections = document.querySelectorAll('.expandable-section');
   var projectsSection = document.getElementById('projects');
-  var projectsButton = document.querySelector('.btn-visit-github'); // Update the selector here
+  var projectsButton = document.querySelector('.btn-visit-github'); 
 
   function handleScroll() {
     var scrollPosition = window.scrollY;
     var windowHeight = window.innerHeight;
 
-    // Handle navbar and scroll-to-top button
     if (scrollPosition > 0) {
       navbar.classList.add('scrolled');
       navbarBrand.classList.remove('expanded');
@@ -21,7 +20,6 @@ document.addEventListener('DOMContentLoaded', function () {
       scrollToTopBtn.classList.remove('visible');
     }
 
-    // Handle expandable sections
     expandableSections.forEach(section => {
       var sectionRect = section.getBoundingClientRect();
       var isInView = sectionRect.top < windowHeight * 0.5 && sectionRect.bottom > windowHeight * 0.5;
@@ -32,7 +30,6 @@ document.addEventListener('DOMContentLoaded', function () {
       section.querySelector('.profile-pic')?.classList.toggle('profile-pic-border-changed', isInView);
     });
 
-    // Handle projects button color change
     if (projectsSection && projectsButton) {
       var projectsRect = projectsSection.getBoundingClientRect();
       var projectsInView = projectsRect.top < windowHeight * 0.5 && projectsRect.bottom > windowHeight * 0.5;
@@ -40,13 +37,9 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  // Initial check
   handleScroll();
-
-  // Add scroll event listener
   window.addEventListener('scroll', handleScroll);
 
-  // Smooth scrolling for anchor links
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -63,7 +56,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  // Scroll to top button functionality
   scrollToTopBtn.addEventListener('click', function (e) {
     e.preventDefault();
     window.scrollTo({
