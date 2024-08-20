442e4f7a6c5b1ff141fa33c00f1a1f5d9bb5d422
justinliu0 <justinliu103@gmail.com>
2024-08-19 21:51:44 -0500

    Create send_mail.php

---
+++ send_mail.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "justinliu103@gmail.com"; 
+    $subject = "New Contact Form Submission: " . $subject;
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent!";
+    } else {
+        echo "Sorry, something went wrong. Please try again.";
+    }
+}
+?>
