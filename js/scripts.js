35dff5500cbf2eb3c3dd7b22fb8c6e27a7ba15b5
justinliu0 <justinliu103@gmail.com>
2025-01-27 09:51:55 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,1 @@
-document.addEventListener("scroll", () => {
-  const projectsContainer = document.querySelector(".projects-sticky");
-  const scrollY = window.scrollY;
 
-  // Add a class when scrolling for the effect
-  if (scrollY > 100) {
-    projectsContainer.classList.add("is-scrolling");
-  } else {
-    projectsContainer.classList.remove("is-scrolling");
-  }
-});
